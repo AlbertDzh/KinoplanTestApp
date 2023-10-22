fbaa56f23c725172d755a2fbd8c9b03a7eb7f985
Albert Dzham <albert_dzham@mail.ru>
2023-10-23 01:54:03 +0300

    added back button functionality

--- app/src/main/java/com/raywenderlich/kinoplantestapp/presentation/screens/releaseRepertoryInfoScreen/ReleaseInfoFragment.kt
+++ app/src/main/java/com/raywenderlich/kinoplantestapp/presentation/screens/releaseRepertoryInfoScreen/ReleaseInfoFragment.kt
@@ -4,11 +4,15 @@ import android.icu.text.CaseMap.Title
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -43,15 +47,34 @@ class ReleaseInfoFragment : Fragment(), LoadReleaseInfoCardView {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
         val toolbarTitle: Toolbar = view.findViewById(R.id.releaseInfoScreenToolbar)!!
         val collapsingImage: ImageView = view.findViewById(R.id.collapsingToolbarImage)
+
+        (activity as? AppCompatActivity)?.let {
+            it.setSupportActionBar(toolbarTitle)
+            it.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
         collapsingImage.loadImage(release.videoThumbnailUrl)
         toolbarTitle.title = release.title
         presenter.convertToValidType(release)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                activity?.onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun loadView(releases: List<ReleaseInfoCardItem>) {
         adapter = ReleaseInfoCardParentAdapter(releases)
         recyclerView.adapter = adapter
     }
+
+
 }
